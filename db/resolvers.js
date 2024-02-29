c55215a92e765d1c1ed671306f6332f24e10f8f4
Pedro Baños <pedrobanos.renault@gmail.com>
2024-02-29 14:13:49 +0100

    done users settings

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,12 +1,65 @@
+const Usuario = require('../models/Usuario')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+require('dotenv').config({ path: 'variables.env' })
 
+const crearToken = (usuario, secreta, expiresIn) => {
+    // console.log((usuario));
+    const { id, email, nombre, apellido } = usuario
+
+    return jwt.sign({ id, email, nombre, apellido }, secreta, { expiresIn })
+}
 // Resolvers
 
 const resolvers = {
     Query: {
-        obtenerCursos: () => "Algo"
+        obtenerUsuario: async (_, { token }) => {
+            const usuarioID = await jwt.verify(token, process.env.SECRETA)
+            
+            return usuarioID
+        }
     },
     Mutation: {
-        nuevoUsuario: () => "Creando un usuario"
+        nuevoUsuario: async (_, { input }) => {
+            const { email, password } = input
+            //Revisar primero si el usuario ya esta registrado
+            const existeUsuario = await Usuario.findOne({ email })
+            if (existeUsuario) {
+                throw new Error('El usuario ya está registrado')
+            }
+            //hashear la password
+            const salt = 10
+            input.password = await bcrypt.hash(password, salt)
+            //guardarlo en la base de datos 
+            try {
+                const usuario = new Usuario(input)
+
+                usuario.save() // lo guarda
+
+                return usuario;
+
+            } catch (error) {
+                console.log(error);
+
+            }
+        },
+        autenticarUsuario: async (_, { input }) => {
+            const { email, password } = input
+            //revisar si el usuario existe
+            const existeUsuario = await Usuario.findOne({ email })
+            if (!existeUsuario) {
+                throw new Error('El usuario no existe')
+            }
+            //Revisar si el password es correcto
+            const passwordCorrecto = await bcrypt.compare(password, existeUsuario.password);
+            if (!passwordCorrecto) {
+                throw new Error('email or password incorrect')
+            }
+            //Crear el token
+            return {
+                token: crearToken(existeUsuario, process.env.SECRETA, '24h')
+            }
+        }
     }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -1,20 +1,35 @@
 const { gql } = require('apollo-server');
 
- const typeDefs= gql`
+const typeDefs = gql`
  
     type Usuario {
         id: ID
         nombre: String
         apellido: String
         email: String
-        creado: String
+        creado: String    
+    }
+    type Token{
+        token: String
+    }
+    input UsuarioInput{
+        nombre: String
+        apellido: String
+        email: String
+        password: String
+
+    }
+    input AutenticarInput{
+        email: String!
+        password: String!
     }
     type Query {
-        obtenerCursos: String
+        obtenerUsuario(token: String!): Usuario
     }
     type Mutation {
-        nuevoUsuario: String
+        nuevoUsuario(input: UsuarioInput): Usuario
+        autenticarUsuario(input: AutenticarInput):  Token
     }
  `;
 
- module.exports = typeDefs;
+module.exports = typeDefs;
